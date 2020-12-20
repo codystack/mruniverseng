cdee6dac63d1dd5b27a364be063ee44f8efe01fa
codystack <ebuzzadvert@gmail.com>
2020-12-20 12:53:52 +0100

    V1

--- admin/components/header.php
+++ admin/components/header.php
@@ -24,7 +24,7 @@ if (isset($_GET['logout'])) {
     <meta property="og:title" content="Mr. Universe Nigeria™ :: Redefining Masculinity">
     <meta property="og:description" content="Mr. Universe Nigeria Contest is organized in a macho way with outdoor activities, public presentations, fun routines and even a chance to meet to impress the ladies.">
     <meta name="keywords" content="ThankGodOkoro.codes,ThankGod Okoro,Vote Online,Mr. Universe Nigeria,Universe Nigeria,beauty pageant,software,voting,election,online contest,contest,award shows,award,shows,pageantry,digital voting,event ticket management,event voting,fashion award,headies awards,amvca,Africa Magic Viewers' Choice Awards,branding">
-    <meta property="og:image" content="https://i.imgur.com/XSGaWUp.png">
+    <meta property="og:image" content="https://i.imgur.com/cwSjIc8.png">
     <meta name="author" content="ThankGod Okoro is a Software developer/programmer, Web + App Developer, UI/UX Designer, Brand Strategist &amp; Product/Graphic Designer.">
     <!-- Favicon -->
     <link rel="shortcut icon" href="https://i.imgur.com/sTG7yhS.png" />
